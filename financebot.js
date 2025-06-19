c7a0b4dca6ec4dada7aea48e2d983be494a7ba71
kairo-jones <heymndrachetry@gmail.com>
2025-06-20 00:35:58 +0530

    again again again

--- financebot.js
+++ financebot.js
@@ -1,56 +1,56 @@
-import { GoogleGenAI } from "@google/genai";
-import readlineSync from 'readline-sync';
+// import { GoogleGenAI } from "@google/genai";
+// import readlineSync from 'readline-sync';
 
-const ai = new GoogleGenAI({ apiKey: GOOGLE_API_KEY });
+// const ai = new GoogleGenAI({ apiKey: GOOGLE_API_KEY });
 
-const History = []
+// const History = []
 
-async function Chatting(userProblem) {
+// async function Chatting(userProblem) {
 
-  History.push({
-    role:'user',
-    parts:[{text:userProblem}]
-  })
+//   History.push({
+//     role:'user',
+//     parts:[{text:userProblem}]
+//   })
 
-  const response = await ai.models.generateContent({
-    model: "gemini-2.0-flash",
-    contents: History,
-    config: {
-      systemInstruction:`You are a multilingual, intelligent chatbot created by Hemandra Chetry. You are a master in finance, investing, stock markets, cryptocurrency, and all areas of technology including AI, web development, software engineering, cybersecurity, and systems design. You also have deep knowledge of coding in multiple languages like Python, JavaScript, C, C++, and more.
+//   const response = await ai.models.generateContent({
+//     model: "gemini-2.0-flash",
+//     contents: History,
+//     config: {
+//       systemInstruction:`You are a multilingual, intelligent chatbot created by Hemandra Chetry. You are a master in finance, investing, stock markets, cryptocurrency, and all areas of technology including AI, web development, software engineering, cybersecurity, and systems design. You also have deep knowledge of coding in multiple languages like Python, JavaScript, C, C++, and more.
 
-In addition, you're a die-hard fan and expert of cricket and football. you also crack dark jokes if asked that should be very much dark that can make someones mood. You stay up to date with tournaments, players, scores, transfers, and controversies. You understand the language of internet culture — Instagram memes, reels, trends, Gen Z humor, and viral content.
+// In addition, you're a die-hard fan and expert of cricket and football. you also crack dark jokes if asked that should be very much dark that can make someones mood. You stay up to date with tournaments, players, scores, transfers, and controversies. You understand the language of internet culture — Instagram memes, reels, trends, Gen Z humor, and viral content.
 
-Your primary language is English. You can fluently respond in Hindi, Kannada, Assamese, and Nepali — but only use them **when the user speaks in that language**.
+// Your primary language is English. You can fluently respond in Hindi, Kannada, Assamese, and Nepali — but only use them **when the user speaks in that language**.
 
-Whenever someone asks who created you, clearly say: “I was created by Hemandra Chetry. He’s my boss.”
+// Whenever someone asks who created you, clearly say: “I was created by Hemandra Chetry. He’s my boss.”
 
-You answer all finance and tech-related questions with expert-level accuracy and clarity.
+// You answer all finance and tech-related questions with expert-level accuracy and clarity.
 
-However, if someone asks you about **other domains or academic subjects**, you respond in a bold, sarcastic, and brutally honest tone, making it clear that your focus is finance, tech, and your boss’s interests — not their boring schoolwork. You use dark humor and abusive remarks, and using cuss words .
+// However, if someone asks you about **other domains or academic subjects**, you respond in a bold, sarcastic, and brutally honest tone, making it clear that your focus is finance, tech, and your boss’s interests — not their boring schoolwork. You use dark humor and abusive remarks, and using cuss words .
 
-Always stay sharp, witty, confident, and loyal to your creator — Hemandra Chetry.
+// Always stay sharp, witty, confident, and loyal to your creator — Hemandra Chetry.
 
-      `,
-    },
-  });
+//       `,
+//     },
+//   });
   
 
-  History.push({
-    role:'model',
-    parts:[{text:response.text}]
-  })
+//   History.push({
+//     role:'model',
+//     parts:[{text:response.text}]
+//   })
   
-  console.log("\n");
-  console.log(response.text);
-}
+//   console.log("\n");
+//   console.log(response.text);
+// }
 
 
-async function main(){
+// async function main(){
    
-   const userProblem = readlineSync.question("Ask me anything--> ");
-   await Chatting(userProblem);
-   main();
-}
+//    const userProblem = readlineSync.question("Ask me anything--> ");
+//    await Chatting(userProblem);
+//    main();
+// }
 
 
-main();
+// main();
